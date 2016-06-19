19836f51d54a1a88fe8ab9e09d7a67b72ffde9b2
Sergey Alyaev <cobxo3bot@gmail.com>
2016-06-19 22:35:29 +0200

    Hacks to better fit data

--- count_steps.m
+++ count_steps.m
@@ -23,13 +23,6 @@ mHalf=floor((m-1)/2);
 alpha_sym = alpha(1+1:1+mHalf,:) + alpha(m:-1:m-mHalf+1,:);
 
 [maxVal, betaHz] = max(alpha_sym);
-median_frequency = median(betaHz)/time;
-if median_frequency<minHz || median_frequency>maxHz
-    steps = 0;
-    stepFreq = 0;
-    betaMagFinal = 0;
-    return
-end
 
 if doPlot
     figure(12)
@@ -38,16 +31,35 @@ if doPlot
     plot(ifft(freq));    
 end
 
+
+%steps = betaHz(maxInd);
+%steps = median(betaHz);
+steps = max(betaHz);
+stepFreq = steps / time;
+
+
 betaMag = zeros(3,1);
 for i=1:3
-    betaMag(i) = 2*sum(alpha_sym(betaHz(i):betaHz(i):mHalf));
-    betaHzHalfI = ceil(betaHz(i)/2);
-    betaMag(i) =  betaMag(i) + sum(alpha_sym(betaHzHalfI:betaHz(i):mHalf));
+    %betaMag(i) = 2*sum(alpha_sym(betaHz(i):betaHz(i):mHalf,i));
+    %betaHzHalfI = ceil(betaHz(i)/2);
+    %betaMag(i) =  betaMag(i) + sum(alpha_sym(betaHzHalfI:betaHz(i):mHalf,i));
+    betaMag(i) = 2*sum(alpha_sym(steps:steps:mHalf,i));
+    betaHzHalfI = ceil(steps/2);
+    betaMag(i) =  betaMag(i) + sum(alpha_sym(betaHzHalfI:steps:mHalf,i));
 end
-betaMagFinal = norm(betaMag, 2)/m
 
-steps = median(betaHz);
-stepFreq = steps / time;
+
+betaMagFinal = norm(betaMag, 2)/m;
+
+[maxVal, maxInd] = max(betaMag);
+
+%if stepFreq<minHz || stepFreq>maxHz
+%    steps = 0;
+%    stepFreq = 0;
+%    betaMagFinal = 0;
+%    return
+%end
+
 
 end
 
--- dependence_on_time.m
+++ dependence_on_time.m
@@ -6,4 +6,9 @@ stepFreq = zeros(m,1);
 magnitude = zeros(m,1);
 for i=rate:m
     [steps(i), stepFreq(i), magnitude(i)] = count_steps(accel_period(1:i,:),i/rate);
+    figure(21)
+    plot(x, stepFreq, x, magnitude/500)
+    figure(23)
+    plot(steps)
 end
+
